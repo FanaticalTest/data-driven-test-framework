4c4351bccdbb5686feeb30920e527930797faf75
Ziridis <samy@kacem.net>
2018-10-01 07:33:29 +0200

    Initial load

---
+++ src/main/java/com/fanaticaltest/datadriventest/DataDrivenTestApplication.java
@@ -0,0 +1,12 @@
+package com.fanaticaltest.datadriventest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDrivenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataDrivenTestApplication.class, args);
+	}
+}
